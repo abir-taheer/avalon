88e8cacf50521ed8bbb94537de7a916a995eb33f
Abir Taheer <abir@taheer.me>
2023-11-19 23:57:47 -0500

    nicer

--- components/game/GameWindow/GameHistory.tsx
+++ components/game/GameWindow/GameHistory.tsx
@@ -8,7 +8,16 @@ import {
   CloseOutlined,
   SkipNextOutlined,
 } from "@mui/icons-material";
-import { List, ListItem, ListItemText, Stack, Typography } from "@mui/material";
+import {
+  FormControl,
+  FormControlLabel,
+  Grid,
+  ListItem,
+  ListItemText,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
 import { useMemo, useState } from "react";
 
 const SuccessIcon = () => <CheckCircleOutline color={"success"} />;
@@ -18,13 +27,19 @@ const SkipIcon = () => <SkipNextOutlined color={"warning"} />;
 
 export const GameHistory = () => {
   const game = useGameContext();
-  const [open, setOpen] = useState(false);
-
+  const [reverse, setReverse] = useState(false);
+  const [open, setOpen] = useState(true);
   const { data } = useRoundsQuery({ game: game.id, skip: !open });
+  const [openAccordionIds, setOpenAccordionIds] = useState<string[]>([]);
 
   const rounds = useMemo(
     () => data?.filter((round) => round.id !== game.currentRoundId) || [],
     [data, game]
+  );
+
+  const render = useMemo(
+    () => (reverse ? [...rounds].reverse() : rounds),
+    [reverse, rounds]
   );
 
   if (game.roundResults.length === 0) {
@@ -48,52 +63,73 @@ export const GameHistory = () => {
         }),
       }}
     >
-      <List>
-        {rounds.map((round) => (
-          <SimpleAccordion
+      <FormControl sx={{ padding: 2 }}>
+        <FormControlLabel
+          control={
+            <Switch checked={reverse} onClick={() => setReverse(!reverse)} />
+          }
+          label={"Reverse Order"}
+        />
+      </FormControl>
+
+      <Grid container spacing={1}>
+        {render.map((round) => (
+          <Grid
+            item
             key={round.id}
-            summary={
-              <Stack direction={"row"} gap={2} alignItems={"center"}>
-                <Typography>Round {round.number}</Typography>
-                {round.status === RoundStatus.team_rejected && <SkipIcon />}
-                {round.status === RoundStatus.team_rejected && (
-                  <Typography variant={"caption"} color={"grey"}>
-                    Team Rejected
-                  </Typography>
-                )}
-
-                {round.status === RoundStatus.mission_failed && <FailIcon />}
-                {round.status === RoundStatus.mission_failed && (
-                  <Typography variant={"caption"} color={"grey"}>
-                    Mission Failed
-                  </Typography>
-                )}
-
-                {round.status === RoundStatus.mission_passed && <SuccessIcon />}
-                {round.status === RoundStatus.mission_passed && (
-                  <Typography variant={"caption"} color={"grey"}>
-                    Mission Passed
-                  </Typography>
-                )}
-              </Stack>
-            }
-            // It will not load the information about the players until the user clicks on the accordion
-            renderDetailsIfHidden={false}
-            initialState={false}
-            accordionProps={{
-              sx: { border: "1px solid rgba(0, 0, 0, 0.12)" },
-            }}
+            xs={12}
+            md={openAccordionIds.includes(round.id) ? 12 : 6}
           >
-            <ListItem key={round.id}>
-              <ListItemText
-                primary={
-                  <RoundVotingResults showDescriptor={false} round={round} />
-                }
-              />
-            </ListItem>
-          </SimpleAccordion>
+            <SimpleAccordion
+              onClose={() =>
+                setOpenAccordionIds((a) => a.filter((b) => b !== round.id))
+              }
+              onOpen={() => setOpenAccordionIds((a) => [...a, round.id])}
+              summary={
+                <Stack direction={"row"} gap={2} alignItems={"center"}>
+                  <Typography>Round {round.number}</Typography>
+                  {round.status === RoundStatus.team_rejected && <SkipIcon />}
+                  {round.status === RoundStatus.team_rejected && (
+                    <Typography variant={"caption"} color={"grey"}>
+                      Team Rejected
+                    </Typography>
+                  )}
+
+                  {round.status === RoundStatus.mission_failed && <FailIcon />}
+                  {round.status === RoundStatus.mission_failed && (
+                    <Typography variant={"caption"} color={"grey"}>
+                      Mission Failed
+                    </Typography>
+                  )}
+
+                  {round.status === RoundStatus.mission_passed && (
+                    <SuccessIcon />
+                  )}
+                  {round.status === RoundStatus.mission_passed && (
+                    <Typography variant={"caption"} color={"grey"}>
+                      Mission Passed
+                    </Typography>
+                  )}
+                </Stack>
+              }
+              // It will not load the information about the players until the user clicks on the accordion
+              renderDetailsIfHidden={false}
+              initialState={false}
+              accordionProps={{
+                sx: { border: "1px solid rgba(0, 0, 0, 0.12)" },
+              }}
+            >
+              <ListItem key={round.id}>
+                <ListItemText
+                  primary={
+                    <RoundVotingResults showDescriptor={false} round={round} />
+                  }
+                />
+              </ListItem>
+            </SimpleAccordion>
+          </Grid>
         ))}
-      </List>
+      </Grid>
     </SimpleAccordion>
   );
 };
